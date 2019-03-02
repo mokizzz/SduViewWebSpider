294b1af3f2600951a6cc55fcdfa82debafde997b
rayiooo <rayiooo@foxmail.com>
2019-03-02 14:07:27 +0800

    修改run_server.py的ip为127.0.0.1:8000（谁改的0.0.0.0:8000？）

--- searchengine/run_server.py
+++ searchengine/run_server.py
@@ -1,7 +1,7 @@
 from os import popen
 
 print('Server opened.')
-result = popen('python manage.py runserver 0.0.0.0:8000')
+result = popen('python manage.py runserver 127.0.0.1:8000')
 res = result.read()
 for line in res.splitlines():
     print(line)
